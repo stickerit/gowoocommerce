6edf5778ad33b1d51cda89ccff4abcedbd4e42cb
Byron Williams <byron@fika.studio>
2023-09-27 15:59:43 +0200

    feat(gowoocommerce): expose signature method

--- webhooks/gowoocommerce.go
+++ webhooks/gowoocommerce.go
@@ -6,12 +6,16 @@ import (
 	"encoding/base64"
 )
 
-func Validate(secret string, xWcWebhookSignature string, b []byte) bool {
+func Signature(secret string, b []byte) string {
 	mac := hmac.New(sha256.New, []byte(secret))
 	mac.Write(b)
 	expectedMAC := mac.Sum(nil)
 
-	b64 := base64.StdEncoding.EncodeToString(expectedMAC)
+	return base64.StdEncoding.EncodeToString(expectedMAC)
+}
+
+func Validate(secret string, xWcWebhookSignature string, b []byte) bool {
+	b64 := Signature(secret, b)
 
 	return b64 == xWcWebhookSignature
 }
